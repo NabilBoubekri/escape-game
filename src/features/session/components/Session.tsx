feca6d0ac17af41870791ffe536bbe8b4fbe3ad7
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-07-03 10:21:07 +0200

    fix: add missing IDs to session data and update key prop in map function

--- src/features/session/components/Session.tsx
+++ src/features/session/components/Session.tsx
@@ -2,6 +2,7 @@ import Card from "./Card";
 
 const data = [
   {
+    id: 1,
     imgUrl:
       "https://lirp.cdn-website.com/1501bd63/dms3rep/multi/opt/ascenseur-escape-game-paris-1920w.jpg",
     title: "L'ASCENSEUR",
@@ -9,6 +10,7 @@ const data = [
       "Détectives mondialement connus, menez votre enquête sur de mystérieuses disparitions dans un ancien immeuble parisien.",
   },
   {
+    id: 2,
     imgUrl:
       "https://irp.cdn-website.com/1501bd63/files/uploaded/LE-MUSEE-V3-WEB-04142ff0.png",
     title: "LE MUSÉE",
@@ -16,6 +18,7 @@ const data = [
       'Incarnez des agents secrets, les "Black King", pour le cambriolage du Diamant Régent dans un musée à la demande d\'un client anonyme. ',
   },
   {
+    id: 3,
     imgUrl:
       "https://irp.cdn-website.com/1501bd63/files/uploaded/LE-PHARAON-V5-WEB-7d17f94f.png",
     title: "LE PHARAON",
@@ -23,6 +26,7 @@ const data = [
       "Aventuriers des temps modernes, partez pour l'exploration d'un tombeau égyptien.",
   },
   {
+    id: 4,
     imgUrl:
       "https://lirp.cdn-website.com/1501bd63/dms3rep/multi/opt/Le+Braquage-poster-6d64d570-1920w.jpg",
     title: "LE BRAQUAGE",
@@ -30,6 +34,7 @@ const data = [
       "Pour cette mission à haute tension, vous devez réaliser un braquage à l'italienne. Objectif de l'opération : voler des lingots d'or !",
   },
   {
+    id: 5,
     imgUrl:
       "https://lirp.cdn-website.com/1501bd63/dms3rep/multi/opt/2023-06-28+00_32_39-LE+CRIME+V3.pdf+-+Foxit+PDF+Reader-1920w.jpg",
     title: "LE CRIME",
@@ -45,8 +50,8 @@ export function Session() {
       <p className="text-center">Vous retrouvez ici toutes nos sessions disponibles!</p>
       <div className="container">
         <div className="row row-cols-1 row-cols-md-3 g-4">
-          {data.map((session, index) => (
-            <div className="col" key={index}>
+          {data.map((session) => (
+            <div className="col" key={session.id}>
               <Card
                 imgUrl={session.imgUrl.toString()}
                 title={session.title}
