e89490c8663a5267245855f70828c61c8a18af5b
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2025-07-04 17:01:56 +0200

    feat: implement dynamic router loading based on environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { HashRouter as Router } from "react-router-dom";
 import './App.css'
+import React, { useEffect, useState } from 'react';
 import AppRoutes from './routes';
+
+function DynamicRouter({ children }: { children: React.ReactNode }) {
+  const [Router, setRouter] = useState<React.ElementType | null>(null);
+
+  useEffect(() => {
+    const loadRouter = async () => {
+      const mod = await import("react-router-dom");
+      setRouter(() =>
+        import.meta.env.MODE === "development"
+          ? mod.BrowserRouter
+          : mod.HashRouter
+      );
+    };
+    loadRouter();
+  }, []);
+
+  if (!Router) return <div>Loading router...</div>;
+
+  return <Router>{children}</Router>;
+}
 
 function App() {
   return (
-   <Router>
+   <DynamicRouter>
       <div className="App">
         <main>
           <AppRoutes />
         </main>
       </div>
-    </Router>
+    </DynamicRouter>
   )
 }
 
